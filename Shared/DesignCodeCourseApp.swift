32bbb02ae28d2c02ea17b77ced0ea3fb87f31b01
Sanket Kumar <sanket.kumar@indwealth.in>
2021-11-05 13:58:55 +0530

    Initial Commit

---
+++ Shared/DesignCodeCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeCourseApp.swift
+//  Shared
+//
+//  Created by sanket kumar on 05/11/21.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
